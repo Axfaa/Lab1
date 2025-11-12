02056d916ce557db67165a0aaee94620da8d47d2
Axfa <Axfa@MSI>
2025-11-12 20:27:31 +0800

    Lab 1

---
+++ Arfa_Lab1/src/Student.java
@@ -0,0 +1,8 @@
+
+public class Student {
+	public static void main(String[] args) {
+        System.out.println("Name: Arfa An-Rusyaidi Bin Mohd Raffidi");
+        System.out.println("Matric Number: BC254726");
+        System.out.println("Address: No.33 Jalan 14/46 Taman Paramount, Petaling Jaya, Selangor");
+    }
+}
